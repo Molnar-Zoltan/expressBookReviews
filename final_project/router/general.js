bf9e7b5cccf2d9693587852fc00a9b55f110f4ce
Molnar-Zoltan <weperdev@gmail.com>
2025-05-28 16:20:37 -0400

    Add route to get book details by Author - Task3

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,8 +27,18 @@ public_users.get('/isbn/:isbn', function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    let booksByAuthor = Object.fromEntries(
+        Object.entries(books).filter(([key, book]) => 
+            book.author.toLowerCase()
+            .startsWith(req.params.author.toLowerCase()))
+    );
+    
+    booksByAuthor
+        ? res.status(200).json(booksByAuthor) 
+        : res.status(404).json({ message: "Book not found" });
+
+    return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get all books based on title
